a5c8dcfd773e6ad405cb9e35b5faf63f9f0a3aa7
윤하겸 <younha3710@naver.com>
2024-12-31 16:23:36 +0900

    commit

--- src/chapter01/HelloJava.java
+++ src/chapter01/HelloJava.java
@@ -1,19 +1,16 @@
 package chapter01;
 
-/**
- * 문서화 주석
- * 작성일 : 2024-12-31
- * 작성자 : 윤하겸
- */
 public class HelloJava{
     public static void main(String[] args) {
         System.out.println("첫 Java 프로그램 입니다."); // 세미콜론 필수
-
-        // 한줄 주석
-        /* 전통적 주석,
-         단축키 Ctrl + Shift + /(슬래쉬) */
-
         System.out.println("화면에 출력하고 있습니다.");
 
+        // 한줄 출력 (끝에 개행)
+        System.out.println("첫 Java 프로그램입니다. 화면에 출력하고 있습니다.");
+
+        // 개행 문자(\n)을 통한 개행 수행
+        System.out.println("첫 Java 프로그램입니다. \n화면에 출력하고 있습니다.");
+
     }
+
 }
