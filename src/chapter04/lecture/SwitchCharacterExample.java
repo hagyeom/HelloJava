be45b4582f46e80b1d554e3b103d5c1f1ae8dbbd
윤하겸 <younha3710@naver.com>
2025-01-07 14:28:22 +0900

    commit

---
+++ src/chapter04/lecture/SwitchCharacterExample.java
@@ -0,0 +1,20 @@
+package chapter04.lecture;
+
+public class SwitchCharacterExample {
+    public static void main(String[] args) {
+        char membershipGrade = 'c';
+
+        switch (membershipGrade) {
+            case 'A':
+            case 'a':
+                System.out.println("우수회원입니다.");
+                break;
+            case 'B':
+            case 'b':
+                System.out.println("일반 회원입니다.");
+                break;
+            default:
+                System.out.println("손님입니다.");
+        }
+    }
+}
