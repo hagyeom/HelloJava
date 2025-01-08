55659d543bb267f6bfec3f75a22d4bc8c1ac76bd
윤하겸 <younha3710@naver.com>
2025-01-08 16:01:42 +0900

    2025-01-08 자바 예제 커밋(chapter 04)

--- src/practice/Problem_04_7.java
+++ src/practice/Problem_04_7.java
@@ -4,37 +4,51 @@ import java.util.Scanner;
 
 public class Problem_04_7 {
     public static void main(String[] args) {
-        boolean run = true;
-        int balance = 0;
-        Scanner scanner = new Scanner(System.in);
 
-        while (run) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean needRun = true;
+
+        int balance = 0;
+
+
+        while (needRun) {
             System.out.println("---------------------------------------");
             System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
             System.out.println("---------------------------------------");
             System.out.print("선택> ");
 
-            int n = scanner.nextInt();
-            if (n == 1) {
-                System.out.print("예금액>");
-                int deposits = scanner.nextInt();
-                balance += deposits;
-                System.out.println();
-            } else if (n == 2) {
-                System.out.print("출금액>");
-                int withdrawal = scanner.nextInt();
-                balance -= withdrawal;
-                System.out.println();
-            } else if (n == 3) {
-                System.out.println("잔고>" + balance);
-                System.out.println();
-            } else if (n == 4) {
-                System.out.println();
-                break;
+            int cmd = sc.nextInt();
 
+            switch (cmd) {
+                case 1: // 예금
+                    System.out.print("예금액> ");
+                    int deposit = sc.nextInt();
+
+                    balance += deposit;
+                    break;
+                case 2: // 출금
+                    System.out.print("출금액> ");
+                    int withdrawl = sc.nextInt();
+
+                    if (balance >= withdrawl) {
+                        balance -= withdrawl;
+                    } else {
+                        System.out.println("잔고가 부족합니다.");
+                    }
+                    break;
+                case 3: // 잔고
+                    System.out.print("잔고> ");
+                    System.out.println(balance);
+                    break;
+                case 4: // 종료
+                    needRun = false;
+                    break;
+                default:
+                    System.out.println("잘못 입력하셨습니다.");
+                    break;
             }
         }
-
         System.out.println("프로그램 종료");
     }
 }
