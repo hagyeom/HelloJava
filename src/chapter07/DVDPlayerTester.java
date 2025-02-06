4aacf889083c1f9cf12e3e8febbdd1bb0b50d42f
윤하겸 <younha3710@naver.com>
2025-02-06 15:25:54 +0900

    2025-02-06 자바 예제 커밋(chapter07, DVD 플레이어)

--- src/chapter07/DVDPlayerTester.java
+++ src/chapter07/DVDPlayerTester.java
@@ -4,7 +4,7 @@ public class DVDPlayerTester {
     public static void main(String[] args) {
         DVDPlayer dvdPlayer = new DVDPlayer();
         dvdPlayer.powerOn();
-        dvdPlayer.insertDVD("세시간짜리영화", 7200); // 세 시간짜리 영화
+        dvdPlayer.insertDVD("두시간짜리영화", 7200); // 두 시간짜리 영화
         dvdPlayer.play();
         dvdPlayer.stop();
         dvdPlayer.ejectDVD();
